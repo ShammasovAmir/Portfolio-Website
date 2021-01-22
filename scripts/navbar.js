cc8ae456960475689a36500d4d2b3d28c210e784
Amir Shammasov <sammasovamir29@gmail.com>
2021-01-22 21:15:53 +0300

    Added animations

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,21 +1,24 @@
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu-list");
-const menuBtn = document.querySelector(".menu-btn");
+const body      = document.querySelector("body");
+const navbar    = document.querySelector(".navbar");
+const menu      = document.querySelector(".menu-list");
+const menuBtn   = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 
-menuBtn.onclick = ()=>{
+menuBtn.onclick = ()=>
+{
     menu.classList.add("active");
     menuBtn.classList.add("hide");
     body.classList.add("disabledScroll");
 }
 
-cancelBtn.onclick = ()=>{
+cancelBtn.onclick = ()=>
+{
     menu.classList.remove("active");
     menuBtn.classList.remove("hide");
     body.classList.remove("disabledScroll");
 }
 
-window.onscroll = ()=>{
+window.onscroll = ()=>
+{
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
